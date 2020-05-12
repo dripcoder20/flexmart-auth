07e7267beaaafa16e67cbebc41fdc65327817e3e
jerud3v <39289253+jerud3v@users.noreply.github.com>
2020-05-12 23:43:52 +0800

    Issue #1 Set up Users field Migration (#10)

--- auth-server/database/migrations/2014_10_12_000000_create_users_table.php
+++ auth-server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,20 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('middle_name')->nullable();
+            $table->string('mobile_number')->unique()->index();
+            $table->string('email')->nullable()->unique();
+            $table->string('house_number')->nullable();
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('province')->nullable();
+            $table->string('zip_code')->nullable();
+            $table->string('nickname')->nullable();
+            $table->string('telephone_number')->nullable();
+            $table->unsignedTinyInteger('status')->default(1);
+            $table->boolean('verified')->default(false);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
